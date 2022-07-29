6bcfb4beb05cf36edd9f02a20627603bbf24244c
MaraOD555 <ruffbc@hotmail.com>
2022-07-29 15:37:33 +0300

    Мастер коммит

---
+++ src/main/java/pro/sky/HW8collections3/Hw8collections3Application.java
@@ -0,0 +1,13 @@
+package pro.sky.HW8collections3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw8collections3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw8collections3Application.class, args);
+	}
+
+}
